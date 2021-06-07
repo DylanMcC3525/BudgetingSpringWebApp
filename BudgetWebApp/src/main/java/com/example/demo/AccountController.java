9bc0a062fb6e39825b8282af2ad14f2a72dc954a
Dylan McCreary <72216783+DylanMcC3525@users.noreply.github.com>
2021-06-07 15:03:00 -0500

    Landing page

---
+++ BudgetWebApp/src/main/java/com/example/demo/AccountController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccountController {
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
