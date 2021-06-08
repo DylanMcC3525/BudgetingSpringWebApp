fb4e8209e69928e1e3702e1a43246ed9424d1c54
Dylan McCreary <72216783+DylanMcC3525@users.noreply.github.com>
2021-06-08 16:14:44 -0500

    database

---
+++ BudgetWebApp/src/main/java/entity/UserRepository.java
@@ -0,0 +1,9 @@
+package entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
---
+++ BudgetWebApp/src/main/java/entity/indexController.java
@@ -0,0 +1,39 @@
+package entity;
+
+import entity.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.security.crypto.bcrypt.*;
+@Controller
+public class indexController {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @GetMapping("/")
+    public String index() {
+
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+
+        return "register";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        userRepo.save(user);
+
+        return "register_success";
+    }
+}
